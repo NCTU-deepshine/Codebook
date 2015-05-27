90c9aac9f6ec9d67acba368a11787f9c7d3a31ae
deepshine <fish83814@yahoo.com.tw>
2015-05-27 15:49:12 +0800

    Convex Hull
    packaged further

--- code/ConvexHull.java
+++ code/ConvexHull.java
@@ -1,57 +1,39 @@
-import java.io.*;
-import java.util.*;
-
-public class Main{
-
-	public static void main(String[] args){
-		Scan scan = new Scan();
-		int testcases = scan.nextInt();
-		while(testcases-- != 0){
-			int n = scan.nextInt();
-			Coordinate[] vertex = new Coordinate[n];
-			for(int i=0;i<n;i++) vertex[i] = new Coordinate(scan.nextDouble(), scan.nextDouble());
-			Arrays.sort(vertex);
-			Coordinate[] list = new Coordinate[n+1];
-			int index = 0;
-			for(int i=0;i<n;i++){
-				while(index >= 2 && ABcrossAC(list[index-2], list[index-1], vertex[i]) <= 0) index--;
-				list[index++] = vertex[i];
-			}
-			int half_point = index+1;
-			for(int i=n-2;i>=0;i--){
-				while(index>=half_point && ABcrossAC(list[index-2], list[index-1], vertex[i])<=0) index--;
-				list[index++] = vertex[i];
-			}
-			double result = 0.0;
-			for(int i=1;i<index;i++){
-				result += Math.sqrt((list[i].x-list[i-1].x)*(list[i].x-list[i-1].x) + (list[i].y-list[i-1].y)*(list[i].y-list[i-1].y));
-			}
-			System.out.println(result);
+static int convexHull(Coordinate[] vertex, Coordinate[] list){
+		int n = vertex.length;
+		Arrays.sort(vertex);
+		int index = 0;
+		for(int i=0;i<n;i++){
+		while(index >= 2 && ABcrossAC(list[index-2], list[index-1], vertex[i]) <= 0) index--;
+		list[index++] = vertex[i];
 		}
-	}
+		int half_point = index+1;
+		for(int i=n-2;i>=0;i--){
+		while(index>=half_point && ABcrossAC(list[index-2], list[index-1], vertex[i])<=0) index--;
+		list[index++] = vertex[i];
+		}
+		return index;
+		}
 
-	static double ABcrossAC(Coordinate A, Coordinate B, Coordinate C){
+static double ABcrossAC(Coordinate A, Coordinate B, Coordinate C){
 		return (B.x-A.x) * (C.y-A.y) - (B.y-A.y) * (C.x-A.x);
+		}
+
+static class Coordinate implements Comparable<Coordinate>{
+
+	double x,y;
+
+	Coordinate(double x, double y){
+		this.x = x;
+		this.y = y;
 	}
 
-	static class Coordinate implements Comparable<Coordinate>{
-
-		double x,y;
-
-		Coordinate(double x, double y){
-			this.x = x;
-			this.y = y;
-		}
-
-		@Override
-		public int compareTo(Coordinate o){
-			if(x < o.x) return -1;
-			if(x > o.x) return 1;
-			if(y < o.y) return -1;
-			if(y > o.y) return 1;
-			return 0;
-		}
-
+	@Override
+	public int compareTo(Coordinate o){
+		if(x < o.x) return -1;
+		if(x > o.x) return 1;
+		if(y < o.y) return -1;
+		if(y > o.y) return 1;
+		return 0;
 	}
 
 }
