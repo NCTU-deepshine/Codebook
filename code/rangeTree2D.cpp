5cd74007081ac718fca74054a77a3551df589eda
Joyqul Chen <joy1357924680@gmail.com>
2015-07-21 21:12:00 +0800

    smaller code size

--- code/rangeTree2D.cpp
+++ code/rangeTree2D.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <cstdio>
-#include <cmath>
-#include <algorithm>
-using namespace std;
-
 struct COORDINATE {
     int x, y;
 };
@@ -16,9 +10,7 @@ bool cmp(const COORDINATE& x, const COORDINATE& y) {
 struct RangeTree2D {
     COORDINATE **container;
     bool **is_left;
-    int **left, **right;
-    int *input;
-    int length, rank, capacity;
+    int **left, **right, *input, length, rank, capacity;
     void init(int *input, int length) {
         this->input = input;
         this->length = length;
@@ -33,13 +25,9 @@ struct RangeTree2D {
             right[i] = new int[capacity];
             is_left[i] = new bool[capacity];
         }
-        for (int i = 0; i < length; ++i) {
-            container[0][i].x = input[i];
+        for (int i = 0; i < capacity; ++i) {
+            container[0][i].x = i>length?0:input[i];
             container[0][i].y = i;
-        }
-        for (int i = length; i < capacity; ++i) {
-            container[0][i].x = 0;
-            container[0][i].y = capacity;
         }
         sort(container[0], container[0]+length, cmp);
         build(rank-1, 0, capacity-1);
@@ -56,8 +44,8 @@ struct RangeTree2D {
         int middle = start+(1<<(height-1));
         build(height-1, start, middle-1);
         build(height-1, middle, finish);
-        int now = start;
-        int l_index = start, r_index = middle;
+        int now = start, l_index = start, r_index = middle;
+
         while (now <= finish) {
 
             left[height][now] = l_index;
@@ -78,35 +66,18 @@ struct RangeTree2D {
         }
     }
 
+    /* 0-base index, k 1-base */
     int query(int start, int finish, int k) {
         return query(rank-1, start, finish, k);
     }
 
     int query(int height, int start, int finish, int k) {
-        //cout << height << " @ " << start << " " << finish << " size: " << k << endl;
         if (height == 0) return container[height][start].x;
         int left_size = left[height][finish] - left[height][start];
         if (is_left[height][finish]) ++left_size;
         int right_size = finish-start+1-left_size;
-        //cout << "size: " << left_size << " " << right_size  << endl;
         if (left_size >= k) return query(height-1, left[height][start], min(left[height][finish], left[height][start]+left_size-1), k);
         else return query(height-1, right[height][start], min(right[height][finish], right[height][start]+right_size-1), k-left_size);
-    }
-
-    void print() {
-        for (int i = rank-1; i >= 0; --i) {
-            for (int j = 0; j < capacity; ++j) {
-                cout << this->container[i][j].x << ":" << this->container[i][j].y << " ";
-            }
-            cout << endl;
-        }
-        cout << "====" << endl;
-        for (int i = rank-1; i >= 0; --i) {
-            for (int j = 0; j < capacity; ++j) {
-                cout << this->left[i][j] << ":" << this->right[i][j] << " ";
-            }
-            cout << endl;
-        }
     }
 };
 
@@ -126,5 +97,4 @@ int main () {
     }
     return 0;
 }
-
 /* Pass POJ 2104 */
